18e5202894b971203272a9887c6a542bfa685b0a
Rob Haining <rob@tolar.ws>
2012-09-09 13:10:28 -0400

    adding hashtag page

--- server.py
+++ server.py
@@ -72,6 +72,10 @@ def welcome():
     return render_template("home.html")
     # return "Hello, world"
 
+@app.route('/twitter')
+def twitter():
+    return render_template("twitter.html")
+
 @app.route('/about/')
 def about():
     return render_template("about.html")
