6b48a3b8a1761f356fa1d48bf7be8e2304fd8c54
aditya <dev@chimeracoder.net>
2012-09-10 22:44:05 -0400

    Remove user registration/login; the functionality is no longer necessary

--- server.py
+++ server.py
@@ -8,7 +8,6 @@ from flask import render_template
 from flask import flash
 from flask import jsonify
 from functools import wraps
-from flask.ext.login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin, AnonymousUser
 import os
 import sys
 import sunlight
@@ -51,9 +50,6 @@ db = MongoAlchemy(app)
 MEMCACHED_TIMEOUT = 10 * 60
 MEMCACHED_TIMEOUT_SUNLIGHT = 24 * 60 * 60
 
-#TODO remove this!
-login_manager = LoginManager()
-login_manager.setup_app(app)
 
 MAX_SEARCH_RESULTS = 20
 
@@ -76,7 +72,6 @@ app.logger.addHandler(file_handler)
 @app.route('/')
 def welcome():
     return render_template("home.html")
-    # return "Hello, world"
 
 @app.route('/twitter')
 def twitter():
@@ -89,126 +84,6 @@ def about():
 @app.route('/contact/')
 def contact():
     return render_template("contact.html")
-
-#This is called *after* the user's password is verified
-#TODO remove the redundant second query and combine it with the first
-@login_manager.user_loader
-def load_user(userid):
-    #TODO check passwords!
-    print("loading user", userid)
-
-    #check the memcached cache first
-    rv = cache.get(userid)
-    if rv is None:
-        rv = MongoUser.query.filter(MongoUser.mongo_id == userid).first()
-        if rv is not None:
-            cache.set(userid, rv, MEMCACHED_TIMEOUT)
-    return rv
-
-def load_user_by_username(username):
-    user_result = MongoUser.query.filter(MongoUser.username == username).first()
-   
-    if user_result is not None:
-        cache.set(str(user_result.mongo_id), user_result, MEMCACHED_TIMEOUT)
-
-    return user_result
-
-
-@app.route('/login/', methods = ["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        #login and validate user
-        login_user(form.user)
-        flash("Logged in successfully")
-        return redirect(request.args.get("next") or url_for("legislators_search"))
-    else:
-
-        return render_template("login.html", form=form)
-
-
-@app.route('/signup/', methods = ["GET" , "POST"])
-def signup():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        print("registering user")
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        confirm = request.form['confirm']
-        zipcode = request.form['zipcode']
-        accept_tos = request.form['accept_tos']
-        new_user = MongoUser(username = username, password = bcrypt.hashpw(password, bcrypt.gensalt()), email = email, zipcode=zipcode, created_at = int(time.time()))
-        new_user.save()
-        return redirect(url_for('welcome'))
-    else:
-        return render_template("signup.html", form=form)
-
-@app.route("/logout/")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('welcome'))
-
-
-
-
-@login_manager.unauthorized_handler
-def unauthorized():
-    return render_template("index.html", flash="unauthorized", intro_text="You need to log in to view this page")
-
-       
-@app.context_processor
-def inject_user_authenticated():
-    return dict(user_authenticated = current_user.is_authenticated())
-    
-
-class MongoUser(db.Document, UserMixin):
-    username = db.StringField()
-    password = db.StringField()
-    zipcode = db.StringField()
-    email = db.StringField()
-    created_at = db.IntField(required = True)
-    
-    def get_id(self):
-        return str(self.mongo_id)
-
-class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=25)])
-    email = TextField('Email Address', [validators.Length(min=6, max=35)])
-    zipcode = TextField('Zipcode', [validators.Length(min=5, max=35)])
-    password = PasswordField('New Password', [
-        validators.Required(),
-        validators.EqualTo('confirm', message='Passwords must match')
-        ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
-
-class LoginForm(Form):
-    username = TextField('Username', [validators.Required()])
-    password = PasswordField('Password', [validators.Required()])
-
-    def validate(self):
-        rv = Form.validate(self)
-        if not rv:
-            return False
-
-        #TODO check password
-        user = MongoUser.query.filter(MongoUser.username == self.username.data).first()
-
-        if user is None:
-            return False
-        else:
-            #self.username = user.username
-            entered_password = self.password.data
-            if bcrypt.hashpw(entered_password, user.password) == user.password:
-                #User entered the correct password
-                cache.set(user.get_id(), user, MEMCACHED_TIMEOUT)
-                self.user = user
-                return True
-            else:
-                return False
-
 
 @app.route('/events/<cid>/')
 @app.route('/events/<cid>/<eid>/')
